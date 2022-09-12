98e4f74264fcff899e5f61ad51131a5ae1812d3a
gxbriel9g <gmendes230@gmail.com>
2022-09-12 12:02:58 -0300

    feat: login page design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,60 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1>Dashgo</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bg="gray.900"
+              variant="filled"
+              _hover={{ bg: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bg="gray.900"
+              variant="filled"
+              _hover={{ bg: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button type="submit" mt={6} colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Home;
